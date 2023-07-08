2cdee3a6923efce43bdab5ce725d0b82eabc48bf
ibrahimhamdan10 <ibrahimkhaledhamdan10@gmail.com>
2023-07-08 13:18:01 -0400

    feature sun has been added

---
+++ src/main/java/org/example/FeatureSUn.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class FeatureSUn {
+
+    String Name;
+    String size;
+    String temp;
+
+//    FeatureSUn Xeon=new FeatureSUn();
+//    Xeon.Name="treo";
+//    Xeon.size="-32432km";
+//    Xeon.temp="-344449843 celcius";
+
+}
